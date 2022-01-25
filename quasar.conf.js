9ef83df8baf6e3d1e3ce61fbe0e290f614a5cccc
Mahery_Rak <mahery0405@gmail.com>
2022-01-25 22:01:54 +0300

    quasar init with pinia

--- quasar.conf.js
+++ quasar.conf.js
@@ -84,7 +84,7 @@ module.exports = configure(function (ctx) {
       open: false, // opens browser window automatically
       proxy: {
         '/api': {
-          target: 'http://localhost:3000',
+          target: 'http://localhost:4000',
           changeOrigin: true,
         },
       },
