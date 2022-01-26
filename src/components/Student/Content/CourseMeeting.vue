a51cbd16688a45f1cec295a089d9b994ac361cb1
Mahery_Rak <mahery0405@gmail.com>
2022-01-26 18:17:26 +0300

    Student

---
+++ src/components/Student/Content/CourseMeeting.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <p>Bienvenue {{ fullName }}</p>
+        <div class="q-pa-md q-gutter-sm">
+            <q-btn color="secondary" label="Participer à la réunion" />
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: '',
+            fullName: '',
+            currentUser: {}
+        }
+    },
+    beforeMount() {
+        /*eslint-disable*/
+        if (!this.$q.sessionStorage.getItem('current_user')) {
+            this.$router.push('/')
+        }
+    },
+    mounted() {
+        /*eslint-disable*/
+        if (!this.$q.sessionStorage.isEmpty()) {
+            this.message = this.$q.sessionStorage.getItem('message');
+            this.fullName = this.$q.sessionStorage.getItem('fullname');
+            this.currentUser = this.$q.sessionStorage.getItem('current_user')
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
