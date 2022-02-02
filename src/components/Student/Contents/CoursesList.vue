66913c84c7d53f8d06a3f7ed06a7c2b11c62e3ab
Sitraka Harinjaka <miarintsoajohary@yahoo.com>
2022-02-02 09:41:54 +0300

    fea: subsrcibing student to course

--- src/components/Student/Contents/CoursesList.vue
+++ src/components/Student/Contents/CoursesList.vue
@@ -12,7 +12,7 @@
           </q-item-label>
           <q-item-label caption lines="2">
             <p>{{ course.description }}</p>
-            <q-btn color="secondary" label="M'inscrire" />
+            <q-btn color="secondary" label="M'inscrire" @click="handleSubscribe(course)"/>
           </q-item-label>
         </q-item-section>
       </q-item>
@@ -21,8 +21,9 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useQuasar } from 'quasar';
+import { useCourseStore } from 'src/stores/course';
 
 export default {
   name: 'CoursesList',
@@ -31,19 +32,49 @@ export default {
     // Plugins import
     const $q = useQuasar();
 
+    // Stores
+    const courseStore = useCourseStore()
+
     // Props
     /*eslint-disable*/
     const { courses } = props;
 
     // States
     let isModalShown = ref(false);
-    let activeCourse = ref({});
-    const token = $q.sessionStorage.getItem('app_token');
+    let token = ref('');
+    let studentId = ref('');
+
+    // Lifecycles 
+    onMounted(() => {
+      token = $q.sessionStorage.getItem('app_token');
+      studentId = $q.sessionStorage.getItem('current_user').user_id
+    })
+
+    // Functions 
+    function handleSubscribe(course) {
+      // When subscribing student
+      courseStore.subscribeToCourse(token, {
+        courseId: course._id,
+        student: studentId
+      }).then((result) => {
+        $q.notify({
+            type: 'positive',
+            message: result.message,
+            position: 'top',
+        });
+      }).catch((error) => {
+        $q.notify({
+            type: 'negative',
+            message: result.message,
+            position: 'top',
+        });
+      })
+    }
 
     return {
       courses,
       isModalShown,
-      activeCourse,
+      handleSubscribe
     };
   },
 };
