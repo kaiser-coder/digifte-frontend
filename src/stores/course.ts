e96bf8846bb38f6001dfef8780996bfbfb1f2414
Mahery_Rak <mahery0405@gmail.com>
2022-02-03 17:12:20 +0300

    store course

---
+++ src/stores/course.ts
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia';
+import { fetchAll, fetchDetails, subscribeStudent, createCourse } from 'src/api/courses';
+
+export const useCourseStore = defineStore('course', {
+    state: () => ({
+        courses: [],
+        activeCourse: {}
+    }),
+    actions: {
+        getAll(appToken: string) {
+            /*eslint-disable*/
+            return fetchAll(appToken);
+        },
+
+        getCourseDetails(appToken: string, courseId: string) {
+            return fetchDetails(appToken, courseId);
+        },
+
+        subscribeToCourse(appToken: string, data: any) {
+            return subscribeStudent(appToken, data);
+        },
+
+        submitCourse(appToken: string, data: any) {
+            return createCourse(appToken, data)
+        }
+    },
+});
