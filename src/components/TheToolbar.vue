8f5ed12faccfd9e72f2f60f3ba41d73a367b6a7e
Sitraka Harinjaka <sitrakaharinjaka@gmail.com>
2022-02-07 05:57:18 +0300

    ref: refactor toolbar

---
+++ src/components/TheToolbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-header elevated> 
+      <q-toolbar :class="color">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="handleMenuClick"
+        />
+
+        <q-toolbar-title>
+          DigiFTE
+        </q-toolbar-title>
+
+        <mail-icon /> &nbsp; &nbsp;
+        <forum-icon /> &nbsp; &nbsp;
+        <account-icon/>
+        
+        <q-icon name="account" class="account">
+          <q-menu>
+            <q-list dense style="min-width: 160px">
+              <q-item clickable v-close-popup>
+                <account-icon/> &nbsp; <q-item-section> Mon profil</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable v-close-popup>
+                <logout-icon/> &nbsp; 
+                <q-item-section @click="dialog = true">
+                  Se déconnecter
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-icon> 
+      </q-toolbar>
+    </q-header>
+
+   <q-dialog v-model="dialog">
+   <q-card>
+      <q-card-section class="row items-center q-gutter-sm">
+         <p>Souhaitez-vous vraiment vous déconnecter ?</p>
+      </q-card-section>
+      <div class="buttonLogoutConfirm" >
+         <q-btn @click="logout" size="10px" style="background: #ffb300;color:white" label="Oui" /> &nbsp; 
+         <q-btn size="10px" style="background: #212121;color:white" label="Non" v-close-popup /> <br> <br>
+      </div>
+   </q-card>
+   </q-dialog> 
+</template>
+
+<script setup>
+   import ForumIcon from 'vue-material-design-icons/Forum.vue';
+   import MailIcon from 'vue-material-design-icons/Mail.vue';
+   import LogoutIcon from 'vue-material-design-icons/Logout.vue';
+   import AccountIcon from 'vue-material-design-icons/Account.vue';
+
+   defineProps({
+      color: String
+   })
+
+   const emit = defineEmits()
+
+   function handleMenuClick() {
+      emit('onMenuClick')
+   }
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,54 +1,10 @@
 <template>
     <q-layout view="lHh Lpr lFf">
-    <q-header elevated> 
-      <q-toolbar :class="toolbarColor">
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleSidebar"
-        />
-
-        <q-toolbar-title>
-          DigiFTE
-        </q-toolbar-title>
-
-        <mail-icon /> &nbsp; &nbsp;
-        <forum-icon /> &nbsp; &nbsp;
-        <account-icon/>
-        
-        <q-icon name="account" class="account">
-          <q-menu>
-            <q-list dense style="min-width: 160px">
-              <q-item clickable v-close-popup>
-                <account-icon/> &nbsp; <q-item-section> Mon profil</q-item-section>
-              </q-item>
-              <q-separator />
-              <q-item clickable v-close-popup>
-                <logout-icon/> &nbsp; 
-                <q-item-section @click="dialog = true">
-                  Se déconnecter
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-icon> 
-      </q-toolbar>
-    </q-header>
-
-    <q-dialog v-model="dialog">
-      <q-card>
-        <q-card-section class="row items-center q-gutter-sm">
-          <p>Souhaitez-vous vraiment vous déconnecter ?</p>
-        </q-card-section>
-        <div class="buttonLogoutConfirm" >
-          <q-btn @click="logout" size="10px" style="background: #ffb300;color:white" label="Oui" /> &nbsp; 
-          <q-btn size="10px" style="background: #212121;color:white" label="Non" v-close-popup /> <br> <br>
-        </div>
-      </q-card>
-    </q-dialog> 
+    <!-- Toolbar -->
+    <TheToolbar 
+      :color="toolbarColor"
+      @onMenuClick="toggleSidebar"
+    />
 
     <!-- Sidebar -->
     <TheSidebar 
@@ -68,11 +24,7 @@
   import { ref, computed } from 'vue';
 
   import TheSidebar from 'src/components/TheSidebar.vue';
-
-  import ForumIcon from 'vue-material-design-icons/Forum.vue';
-  import MailIcon from 'vue-material-design-icons/Mail.vue';
-  import LogoutIcon from 'vue-material-design-icons/Logout.vue';
-  import AccountIcon from 'vue-material-design-icons/Account.vue';
+  import TheToolbar from 'src/components/TheToolbar.vue';
 
   const isOpen = ref(false);
   const menuItems = ref({
@@ -129,7 +81,7 @@
   })
 
   function toggleSidebar() {
-    console.log('toggle sidebar')
+    console.log('toggle sidebar') 
     isOpen.value = !isOpen.value 
   }
 </script>
