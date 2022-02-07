b6e5e7ebf412558731e7b37497706da10a10466e
Mahery_Rak <mahery0405@gmail.com>
2022-02-07 13:56:31 +0300

    config port

--- quasar.conf.js
+++ quasar.conf.js
@@ -82,12 +82,12 @@ module.exports = configure(function (ctx) {
       },
       port: 8080,
       open: true, // opens browser window automatically
-      // proxy: {
-      //   '/api': {
-      //     target: 'http://localhost:3000',
-      //     changeOrigin: true,
-      //   },
-      // },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      },
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
