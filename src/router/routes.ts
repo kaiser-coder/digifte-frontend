f2b93a0cf89c12ec1560010a7ef6027f8461d6d5
Sitraka Harinjaka <sitraka.rakotobe@crossfound.com>
2022-02-09 13:42:28 +0300

    ref(route): Refactor routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -29,29 +29,18 @@ const routes: RouteRecordRaw[] = [
           { path: 'list', component: () => import('pages/Shared/CoursesList.vue') },
           { path: 'details/:courseId', component: () => import('pages/Shared/DetailsCourse.vue') },
         ]
+      },
+      {
+        path: 'students',
+        component: () => import('layouts/MainLayout.vue'),
+        children: [
+          { path: 'follow', component: () => import('pages/Administrator/FollowStudent.vue') },
+          { path: 'register', component: () => import('pages/Administrator/RegistrationManagement.vue') },
+          { path: 'agenda', component: () => import('pages/Student/StudentAgenda.vue') }
+        ]
       }
     ]
-  },
-  
-  /*
-  {
-    path: '/home-admnistrator',
-    component: () => import('src/components/Admnistrator/HomePageAdmnistrator.vue'),
-    children: [
-      { path: 'student-follow', component: () => import('src/components/Admnistrator/Contents/FollowStudent.vue')},
-      { path: 'registration-management', component: () => import('src/components/Admnistrator/Contents/RegistrationManagement.vue')},
-    ]
-  },
-
-  {
-    path: '/home-student',
-    component: () => import('src/components/Student/HomePageStudent.vue'),
-    children: [
-      { path: 'student-subscribe', component: () => import('src/components/Student/Contents/StudentCourseMeeting.vue')},
-      { path: 'student-agenda', component: () => import('src/components/Student/Contents/StudentAgenda.vue') },
-      { path: 'course-details/:courseId', component: () => import('src/components/Student/Contents/CourseDetails.vue')}
-    ]
-  }, */
+  }
 ];
 
 export default routes;
