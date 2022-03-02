c537be50439cd72af243e1c3b7ac947cc6608a08
Mahery_Rak <mahery0405@gmail.com>
2022-03-02 11:24:13 +0300

    Fix bug for calendar transition

--- src/components/Agenda/TheCalendarWeek.vue
+++ src/components/Agenda/TheCalendarWeek.vue
@@ -1,10 +1,10 @@
-<template lang="" >
+<template lang="">
   <div class="subcontent">
-  <q-btn-group>
-    <q-btn @click="onToday" color="blue-grey-4" glossy text-color="white" push label="Aujourd'hui" />
-    <q-btn @click="onPrev" color="blue-grey-4" glossy text-color="white" push label="Prev" />
-    <q-btn @click="onNext" color="blue-grey-4" glossy text-color="white" push label="Next" />
-  </q-btn-group> &nbsp; <br> <br>
+    <q-btn-group>
+      <q-btn @click="onToday" color="blue-grey-4" glossy text-color="white" push label="Aujourd'hui" />
+      <q-btn label="Prev" icon="navigate_before" @click="onPrev" color="blue-grey-4" glossy text-color="white" /> 
+      <q-btn label="Next" icon-right="navigate_next" @click="onNext" color="blue-grey-4" glossy text-color="white"  />
+    </q-btn-group> &nbsp; <br> <br>
 
     <div class="row justify-center">
       <div style="display: flex; max-width: 250%; width: 100%; height: 400px;">
@@ -41,15 +41,10 @@
                   style="width: 100%; cursor: pointer; height: 12px; font-size: 10px; margin: 1px;"
                 >
                   <span class="title q-calendar__ellipsis">
-                    {{ event.title }}  &nbsp;
-                    <q-icon
-                      name="launch"
-                      @click="launchMeeting(event.details.join_url)"
-                      v-if="event.details !== undefined"
-                    />
+                    {{ event.title }}
+                    <q-tooltip>{{ event.details }}</q-tooltip>
                   </span>
                 </q-badge>
-
                 <q-badge
                   v-else
                   :class="badgeClasses(event, 'header')"
@@ -57,7 +52,7 @@
                   style="margin: 1px; width: 10px; max-width: 10px; height: 10px; max-height: 10px"
                   @click="scrollToEvent(event)"
                 >
-                <q-tooltip>{{ event.time + ' - ' + event.details }}</q-tooltip>
+                  <q-tooltip>{{ event.time + ' - ' + event.details }}</q-tooltip>
                 </q-badge>
               </template>
             </div>
@@ -76,13 +71,7 @@
               >
                 <span class="title q-calendar__ellipsis">
                   {{ event.title }}
-                  <q-tooltip>
-                    <ul style="list-style: none;">
-                      <li>Durée: {{ event.details[0] }}</li>
-                      <li>Lien du Meeting: {{ event.details[1] }}</li>
-                      <li>Date de début: {{ event.details[2] }}</li>
-                    </ul>
-                  </q-tooltip>
+                  <q-tooltip>{{ event.details }}</q-tooltip>
                 </span>
               </div>
             </template>
@@ -93,7 +82,7 @@
   </div>
 </template>
 
-<script setup>
+<script>
 /*eslint-disable*/
 import {
   QCalendarDay,
@@ -108,10 +97,8 @@ import {
 import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarDay.sass'
-
-import { computed, onMounted, ref } from 'vue';
-
-const emits = defineEmits(['onLaunchMeeting']);
+import { defineComponent } from 'vue'
+//import NavigationBar from '../components/NavigationBar.vue'
 
 // The function below is used to set up our demo data
 const CURRENT_DAY = new Date()
@@ -121,165 +108,150 @@ function getCurrentDay (day) {
   const tm = parseDate(newDay)
   return tm.date
 }
-
-const selectedDate = today();
-const props = defineProps({
-  lessons: Array
-});
-
-const handleTime = (dateD) => {
-  let hrs = dateD.getHours()
-  let mins = dateD.getMinutes()
-  if(hrs<=9)
-      hrs = '0' + hrs
-  if(mins<10)
-    mins = '0' + mins
-  const postTime= hrs + ':' + mins
-  return postTime
-}
-
-const events = computed(() => {
-  // Format data
-  let custom = [];
-  props.lessons.forEach((d) => {
-    const date = new Date(d.start_date);
-    // console.log('Time => ', handleTime(date))
-    custom.push({
-      id: d._id,
-      title: d.name,
-      details: d.meeting,
-      date: getCurrentDay(date.getDate()), // start_date
-      time: handleTime(date),
-      duration: d.meeting ? d.meeting.duration : 1,
-      bgcolor: d.bgcolor
-    })
-  })
-  return custom
-});
-
-const calendar = ref(null)
-
-onMounted(() => {
-  console.log('Lessons => ', props.lessons);
-  console.log('CAL MOUNT ', calendar.value);
-  onToday();
-  onPrev();
-  onNext();
-})
-
-// Dispatch events by date
-const eventsMap = computed (() => {
-    const map = {}
-    events.value.forEach(event => {
-    if (!map[ event.date ]) {
-      map[ event.date ] = []
-    }
-    map[ event.date ].push(event)
-    if (event.days) {
-        let timestamp = parseTimestamp(event.date)
-        let days = event.days
-        do {
-          timestamp = addToDate(timestamp, { day: 1 })
-          if (!map[ timestamp.date ]) {
-              map[ timestamp.date ] = []
-          }
-          map[ timestamp.date ].push(event)
-        } while (--days > 0)
-    }
-    })
-    console.log('Mapped => ', map)
-    return map
-})
-
-function badgeClasses (event, type) {
-    const isHeader = type === 'header'
+export default defineComponent({
+  name: 'WeekSlotDayBody',
+  components: {
+    QCalendarDay
+  },
+  props: ['lessons'],
+  data () {
     return {
-    [ `text-white bg-${ event.bgcolor }` ]: true,
-    'full-width': !isHeader && (!event.side || event.side === 'full'),
-    'left-side': !isHeader && event.side === 'left',
-    'right-side': !isHeader && event.side === 'right',
-    'rounded-border': true
+      selectedDate: today(),
+      events: []
     }
-}
+  },
+  computed: {
+    // convert the events into a map of lists keyed by date
+    eventsMap () {
+      const map = {}
+      // this.events.forEach(event => (map[ event.date ] = map[ event.date ] || []).push(event))
+      this.events.forEach(event => {
+        if (!map[ event.date ]) {
+          map[ event.date ] = []
+        }
+        map[ event.date ].push(event)
+        if (event.days) {
+          let timestamp = parseTimestamp(event.date)
+          let days = event.days
+          do {
+            timestamp = addToDate(timestamp, { day: 1 })
+            if (!map[ timestamp.date ]) {
+              map[ timestamp.date ] = []
+            }
+            map[ timestamp.date ].push(event)
+          } while (--days > 0)
+        }
+      })
+      return map
+    },
+  },
 
-function badgeStyles (event, type, timeStartPos = undefined, timeDurationHeight = undefined) {
-    const s = {}
-    if (timeStartPos && timeDurationHeight) {
-    s.top = timeStartPos(event.time) + 'px'
-    s.height = timeDurationHeight(event.duration) + 'px'
+  mounted() {
+    function handleTime(dateD) {
+      let hrs = dateD.getHours()
+      let mins = dateD.getMinutes()
+      if(hrs<=9)
+        hrs = '0' + hrs
+      if(mins<10)
+        mins = '0' + mins
+      const postTime= hrs + ':' + mins
+      return postTime
     }
-    s[ 'align-items' ] = 'flex-start'
-    return s
-}
 
-function getEvents (dt) {
-    // get all events for the specified date
-    const events = eventsMap.value[dt] || []
-    // console.log('Date event => ', dt);
-
-    if (events.length === 1) {
-      events[0].side = 'full'
-    }
-    else if (events.length === 2) {
-      // this example does no more than 2 events per day
-      // check if the two events overlap and if so, select
-      // left or right side alignment to prevent overlap
-      const startTime = addToDate(parsed(events[0].date), { minute: parseTime(events[0].time) })
-      const endTime = addToDate(startTime, { minute: events[0].duration })
-      const startTime2 = addToDate(parsed(events[1].date), { minute: parseTime(events[1].time) })
-      const endTime2 = addToDate(startTime2, { minute: events[1].duration })
-      if (isBetweenDates(startTime2, startTime, endTime, true) || isBetweenDates(endTime2, startTime, endTime, true)) {
-          events[0].side = 'left'
-          events[1].side = 'right'
+    this.lessons.forEach((d) => {
+      const date = new Date(d.start_date);
+      // console.log('Time => ', handleTime(date))
+      this.events.push({
+        id: d._id,
+        title: d.name,
+        details: d.meeting,
+        date: getCurrentDay(date.getDate()), // start_date
+        time: handleTime(date),
+        duration: d.meeting ? d.meeting.duration : 1,
+        bgcolor: d.bgcolor
+      })
+    })
+  },
+  methods: {
+    badgeClasses (event, type) {
+      const isHeader = type === 'header'
+      return {
+        [ `text-white bg-${ event.bgcolor }` ]: true,
+        'full-width': !isHeader && (!event.side || event.side === 'full'),
+        'left-side': !isHeader && event.side === 'left',
+        'right-side': !isHeader && event.side === 'right',
+        'rounded-border': true
       }
-      else {
-          events[0].side = 'full'
-          events[1].side = 'full'
+    },
+    badgeStyles (event, type, timeStartPos = undefined, timeDurationHeight = undefined) {
+      const s = {}
+      if (timeStartPos && timeDurationHeight) {
+        s.top = timeStartPos(event.time) + 'px'
+        s.height = timeDurationHeight(event.duration) + 'px'
       }
+      s[ 'align-items' ] = 'flex-start'
+      return s
+    },
+    getEvents (dt) {
+      // get all events for the specified date
+      const events = this.eventsMap[ dt ] || []
+      if (events.length === 1) {
+        events[ 0 ].side = 'full'
+      }
+      else if (events.length === 2) {
+        // this example does no more than 2 events per day
+        // check if the two events overlap and if so, select
+        // left or right side alignment to prevent overlap
+        const startTime = addToDate(parsed(events[ 0 ].date), { minute: parseTime(events[ 0 ].time) })
+        const endTime = addToDate(startTime, { minute: events[ 0 ].duration })
+        const startTime2 = addToDate(parsed(events[ 1 ].date), { minute: parseTime(events[ 1 ].time) })
+        const endTime2 = addToDate(startTime2, { minute: events[ 1 ].duration })
+        if (isBetweenDates(startTime2, startTime, endTime, true) || isBetweenDates(endTime2, startTime, endTime, true)) {
+          events[ 0 ].side = 'left'
+          events[ 1 ].side = 'right'
+        }
+        else {
+          events[ 0 ].side = 'full'
+          events[ 1 ].side = 'full'
+        }
+      }
+      return events
+    },
+    scrollToEvent (event) {
+      this.$refs.calendar.scrollToTime(event.time, 350)
+    },
+    onToday () {
+      this.$refs.calendar.moveToToday()
+    },
+    onPrev () {
+      this.$refs.calendar.prev()
+    },
+    onNext () {
+      this.$refs.calendar.next()
+    },
+    onMoved (data) {
+      console.log('onMoved', data)
+    },
+    onChange (data) {
+      console.log('onChange', data)
+    },
+    onClickDate (data) {
+      console.log('onClickDate', data)
+    },
+    onClickTime (data) {
+      console.log('onClickTime', data)
+    },
+    onClickInterval (data) {
+      console.log('onClickInterval', data)
+    },
+    onClickHeadIntervals (data) {
+      console.log('onClickHeadIntervals', data)
+    },
+    onClickHeadDay (data) {
+      console.log('onClickHeadDay', data)
     }
-    return events
-}
-
-function scrollToEvent (event) {
-  calendar.value.scrollToTime(event.time, 350)
-}
-function onToday () {
-  calendar.value.moveToToday()
-}
-function onPrev () {
-  calendar.value.prev()
-}
-function onNext () {
-  calendar.value.next()
-}
-
-function onMoved (custom) {
-  console.log('onMoved', custom)
-}
-function onChange (custom) {
-  console.log('onChange', custom)
-}
-function onClickDate (custom) {
-  console.log('onClickDate', custom)
-}
-function onClickTime (custom) {
-  console.log('onClickTime', custom)
-}
-function onClickInterval (custom) {
-  console.log('onClickInterval', custom)
-}
-function onClickHeadIntervals (custom) {
-  console.log('onClickHeadIntervals', custom)
-}
-function onClickHeadDay (custom) {
-  console.log('onClickHeadDay', custom)
-}
-
-function launchMeeting(url) {
-  emits('onLaunchMeeting', url)
-}
-
-
+  }
+})
 </script>
 
 <style lang="sass" scoped>
@@ -291,50 +263,37 @@ function launchMeeting(url) {
   text-overflow: ellipsis
   overflow: hidden
   cursor: pointer
-
 .title
   position: relative
   display: flex
   justify-content: center
   align-items: center
   height: 100%
-
 .text-white
   color: white
-
 .bg-blue
   background: blue
-
 .bg-green
   background: green
-
 .bg-orange
   background: orange
-
 .bg-red
   background: red
-
 .bg-teal
   background: teal
-
 .bg-grey
   background: grey
-
 .bg-purple
   background: purple
-
 .full-width
   left: 0
   width: calc(100% - 2px)
-
 .left-side
   left: 0
   width: calc(50% - 3px)
-
 .right-side
   left: 50%
   width: calc(50% - 3px)
-
 .rounded-border
   border-radius: 2px
 </style>
