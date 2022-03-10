47422f90137f47a713b3ee6c2ee8344f0d8930d4
Mahery_Rak <mahery0405@gmail.com>
2022-03-10 11:39:12 +0300

    Fix defines

--- src/components/Agenda/ListsCourses.vue
+++ src/components/Agenda/ListsCourses.vue
@@ -1,20 +1,39 @@
 <template lang="">
-    <div class="row">
-      <q-btn class="q-ma-xs" :color="course.bgcolor" :label="course.title" v-for="course in courses" :key="course._id" @click="handleClick(course._id)" :outline="false" ref="btn"/>
-    </div>
+  <div class="row">
+    <q-btn
+      class="q-ma-xs"
+      :color="course.bgcolor"
+      :label="course.title" v-for="course in btns" :key="course._id"
+      @click="handleClick(course._id)"
+      ref="btn"
+    />
+  </div>
 </template>
 
 <script setup>
+/*eslint-disable*/
+  import { defineEmits, computed } from 'vue';
   const props = defineProps({
     courses: Array
   });
-
   const emits = defineEmits()
-
+  const btns = computed(() => {
+    let result = []
+    props.courses.forEach((c) => {
+      result.push({ ...c, state: true })
+    })
+    console.log(result)
+    return result
+  })
   console.log('Courses list => ', props.courses)
-
   function handleClick(courseId) {
-    /*eslint-disable*/
+    btns.value.forEach((btn) => {
+      if(btn._id !== courseId) {
+        btn.state = false
+        btn.bgcolor = 'grey'
+      }
+    })
+    console.log('Btns states => ', btns.value)
     emits('onViewLessons', courseId)
   }
 </script>
