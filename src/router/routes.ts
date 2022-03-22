06bb29685d22414ca4e3beeef72e92d762ed2c81
Mahery_Rak <mahery0405@gmail.com>
2022-03-22 17:44:43 +0300

    Update route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -37,7 +37,12 @@ const routes: RouteRecordRaw[] = [
           { path: 'follow', component: () => import('pages/Admnistrator/FollowStudent.vue') },
           { path: 'register', component: () => import('pages/Admnistrator/RegistrationManagement.vue') },
           { path: 'agenda', component: () => import('pages/Student/StudentAgenda.vue') },
-          { path: 'lists', component: () => import('src/pages/Shared/StudentsLists.vue') }
+          { path: 'administratif', component: () => import('src/pages/Student/StudentAdministratif.vue'),
+            children : [ 
+              { path: 'bulletin', component:() => import('src/components/Administration/Bulletin.vue') }
+            ]
+          },
+          { path: 'lists', component: () => import('src/pages/Shared/StudentsLists.vue') },
         ]
       },
     ]
