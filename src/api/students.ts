eace11e5a532869bac0b5bd8e93a944bc3bf996c
Sitraka Harinjaka <sitraka.rakotobe@crossfound.com>
2022-04-27 14:24:09 +0300

    fea: build edit profile form

--- src/api/students.ts
+++ src/api/students.ts
@@ -1,11 +1,32 @@
 /*eslint-disable*/
-
 import axios from 'axios';
 
 const fetchAll = async (appToken: string, sfToken: string): Promise<any> => {
-    const response = await axios.get(`/api/app/students/${sfToken}`, { headers: { 'x-access-token': appToken } });
-    // console.log('Response from students', response )
-    return response;
-}
+  const response = await axios.get(`/api/app/students/${sfToken}`, {
+    headers: { 'x-access-token': appToken },
+  });
+  // console.log('Response from students', response )
+  return response;
+};
 
-export { fetchAll }
+const getOne = async (appToken: string, sfToken: string, sfId: string): Promise<any> => {
+  const response = await axios.get(`/api/salesforce/contacts/${sfId}`, {
+    headers: { 
+      'x-access-token': appToken,
+      'sf_token': sfToken
+    },
+  });
+  return response.data;
+};
+
+const update = async (appToken: string, sfToken: string, sfId: string): Promise<any> => {
+  const response = await axios.patch(`/api/salesforce/contacts/${sfId}`, {
+    headers: { 
+      'x-access-token': appToken,
+      'sf_token': sfToken
+    },
+  });
+  return response.data;
+};
+
+export { fetchAll, getOne, update };
